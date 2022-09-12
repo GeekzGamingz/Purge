6cff6474bbcfa47255fa09e2ff985df99c9c5988
GeekzGamingz <geekzgamingz@gmail.com>
2022-09-12 06:44:26 -0400

    Implemented Boomshot

---
+++ source/03_Objects/01_Projectiles/02_Slug/Slug.gd
@@ -0,0 +1,28 @@
+#Inherits KinObject Code
+extends KinObject
+#------------------------------------------------------------------------------#
+#Variables
+var colliding = false
+#------------------------------------------------------------------------------#
+#Ready
+func _ready() -> void:
+	$SelfDestruct.start()
+#------------------------------------------------------------------------------#
+#Process
+func _process(_delta: float) -> void:
+	if colliding:
+		get_parent().remove_child(self)
+		queue_free()
+#Physics Process
+func _physics_process(_delta: float) -> void:
+	motion = move_and_slide(motion)
+#------------------------------------------------------------------------------#
+#Self Destruct
+func _on_SelfDestruct_timeout() -> void:
+	colliding = true
+func _on_MediumAttack_body_entered(body: Node) -> void:
+	if body.name != "Player":
+		motion = Vector2.ZERO
+		$AnimationPlayer.play("impact")
+		yield($AnimationPlayer, "animation_finished")
+		colliding = true
